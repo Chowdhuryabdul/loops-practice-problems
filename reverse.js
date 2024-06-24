73563468a9ae706156ed5213c7eced7f2f55eca4
Md Abdul Quaiyum Chowdhury <Md.Abdul.Quaiyum.Chowdhury@bodo.kommune.no>
2024-06-24 22:01:07 +0200

    last

---
+++ reverse.js
@@ -0,0 +1,19 @@
+/* var number = 1;
+while(number <= 10){
+    console.log(number)
+    number++
+} */
+/* 
+var number = 10;
+while(number >= 1){
+    console.log(number)
+    number--;
+} */
+
+// for(var i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+for(var i = 10; i >=1; i--){
+    console.log(i)
+}
