eff43e2942fe1abe3787475653aeb58dfd7d7730
Md Abdul Quaiyum Chowdhury <Md.Abdul.Quaiyum.Chowdhury@bodo.kommune.no>
2024-06-24 17:01:10 +0200

    commit

--- continue.js
+++ continue.js
@@ -1,3 +1,4 @@
+// continue it means to skip the numbers. it means the if the condition number is 100 it will skip all the numbers bigger than 100.
 var numbers = [12, 14, 50, 111, 30, 35, 50]
 for(var i = 0; i < numbers.length; i++){
     var number = numbers[i];
